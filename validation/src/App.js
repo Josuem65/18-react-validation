b9a2201a0964b7707ead7e9222bef2883b7d9c74
Josue Martinez <josue.martinez65@yahoo.com>
2020-10-04 20:18:29 -0700

    finished

---
+++ validation/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Form from "./Form"
+import'./App.css'
+// import validator from 'validator';
+
+function App() {
+    return (
+      <div className="formContainer">
+        <Form />
+      </div>
+  )
+}
+
+export default App
